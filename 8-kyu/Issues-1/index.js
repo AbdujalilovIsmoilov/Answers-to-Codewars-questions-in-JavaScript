827e0bf3e34a25d026f47b32b9e6061605d37206
= <ismoilbek20050421@gmail.com>
2023-01-24 15:24:33 +0500

    Codewars to JavaScript || 8-kyu || Issues: N1

---
+++ 8-kyu/Issues-1/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// 8-kyu: Issues-1
+
+function evenOrOdd(number) {
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+evenOrOdd(2);
+
+// #### Create a function that takes an integer as an argument and returns
+// #### "Even" for even numbers or "Odd" for odd numbers.
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
