bab9852bd777909a5cc4cf69db6f80715ec8e114
= <ismoilbek20050421@gmail.com>
2023-01-26 14:49:51 +0500

    Codewars to JavaScript || 8-kyu || Issues: N4

---
+++ 8-kyu/Issues-4/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// 8-kyu: Issues-4
+// #### You Can't Code Under Pressure #1
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i ** 2;
+}
+doubleInteger(10);
+
+// #### Code as fast as you can! You need to double the integer and return it.
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
