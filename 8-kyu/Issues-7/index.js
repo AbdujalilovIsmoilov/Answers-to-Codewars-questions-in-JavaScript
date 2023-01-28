3572b26952ff822f190ca78e1b39465f1e78953a
= <ismoilbek20050421@gmail.com>
2023-01-29 02:26:11 +0500

    Codewars to JavaScript || 8-kyu || Issues: N7

--- 8-kyu/Issues-7/index.js
+++ 8-kyu/Issues-7/index.js
@@ -6,7 +6,6 @@
 const greet = (name, owner) => name == owner ? "Hello boss" : "Hello guest";
 greet("Daniel", "Daniel");
 
-
 // ### Create a function that gives a personalized greeting. 
 // ### This function takes two parameters: name and owner.
 
