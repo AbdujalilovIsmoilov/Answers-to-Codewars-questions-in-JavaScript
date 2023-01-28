323eeb5260cc8d08bcf60e7e281ffe5bd1c86a9a
= <ismoilbek20050421@gmail.com>
2023-01-29 02:24:10 +0500

    Codewars to JavaScript || 8-kyu || Issues: N7

---
+++ 8-kyu/Issues-7/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// 8-kyu: Issues-7
+// #### Grasshopper - Personalized Message
+
+const greet = (name, owner) => name == owner ? "Hello boss" : "Hello guest";
+greet("Daniel", "Daniel");
+
+/* 
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+*/
+
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/javascript
