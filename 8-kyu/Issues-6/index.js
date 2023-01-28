ebcf7954e43f1bde820ad2ff1f32972793305708
= <ismoilbek20050421@gmail.com>
2023-01-28 16:09:59 +0500

    Codewars to JavaScript || 8-kyu || Issues: N6

---
+++ 8-kyu/Issues-6/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// 8-kyu: Issues-6
+// #### Convert a Boolean to a String
+
+const booleanToString = (b) => String(b);
+booleanToString(10);
+
+/* 
+Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given.
+*/
+
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
