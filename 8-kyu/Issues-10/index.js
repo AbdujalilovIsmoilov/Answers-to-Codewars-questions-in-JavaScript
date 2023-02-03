a0032ddc2c06b01fa4bfdb3774eb63a14faa2697
= <ismoilbek20050421@gmail.com>
2023-02-03 23:22:46 +0500

    Codewars to JavaScript || 8-kyu || Issues: N10

---
+++ 8-kyu/Issues-10/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// 8-kyu: Issues-10
+// #### Grasshopper - Function syntax debugging
+
+const main = (verb, noun) => verb + noun;
+
+// ### A student was working on a function and made some syntax mistakes while coding.
+// ### Help them find their mistakes and fix them.
+
+
+// https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/javascript
