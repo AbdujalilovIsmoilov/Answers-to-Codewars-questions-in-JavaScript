7c20effb208831ed335be03f73ec9b41b605b7c8
= <ismoilbek20050421@gmail.com>
2023-02-07 00:19:26 +0500

    Codewars to JavaScript || 8-kyu || Issues: N11

---
+++ 8-kyu/Issues-11/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// 8-kyu: Issues-11
+// #### Return to Sanity
+
+function mystery() {
+  return { sanity: "Hello" };
+}
+mystery();
+
+// ### This function should return an object, but it's not doing what's intended.
+// ### What's wrong?
+
+// https://www.codewars.com/kata/514a7ac1a33775cbb500001e/train/javascript
