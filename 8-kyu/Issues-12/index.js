84ffa2f3f5fabfc971d33902d5370816faf0cae9
= <ismoilbek20050421@gmail.com>
2023-02-07 00:42:58 +0500

    Codewars to JavaScript || 8-kyu || Issues: N12

---
+++ 8-kyu/Issues-12/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// 8-kyu: Issues-12
+// #### Reversed Strings
+
+const solution = (str) => str.split("").reverse().join("")
+solution("word");
+
+// ### Complete the solution so that it reverses the string passed into it.
+
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
