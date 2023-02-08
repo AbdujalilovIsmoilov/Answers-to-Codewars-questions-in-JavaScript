4895c6cc5a655fa7f30c2b328042de9df78fef1f
= <ismoilbek20050421@gmail.com>
2023-02-08 19:26:48 +0500

    Codewars to JavaScript || 8-kyu || Issues: N13

---
+++ 8-kyu/Issues-13/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// 8-kyu: Issues-13
+// #### Name Shuffler
+
+const nameShuffler = (str) => str.split(" ").reverse().join(" ");
+nameShuffler("john McClane");
+
+// ### Write a function that returns a string in which firstname is swapped with last name.
+// ### Example(Input --> Output)
+// ### "john McClane" --> "McClane john"
+
+// https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript
