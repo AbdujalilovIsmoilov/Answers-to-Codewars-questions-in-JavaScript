82da158f5e28d564693f8cb361e0db9b40ee9018
= <ismoilbek20050421@gmail.com>
2023-02-08 22:48:50 +0500

    Codewars to JavaScript || 6-kyu || Issues: N1

--- 6-kyu/Issues-1/index.js
+++ 6-kyu/Issues-1/index.js
@@ -11,6 +11,8 @@ function createPhoneNumber(numbers) {
   return array;
 }
 
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+
 // ### Write a function that accepts an array of 10 integers (between 0 and
 // ### 9), that returns a string of those numbers in the form of a phone
 // ### number.
