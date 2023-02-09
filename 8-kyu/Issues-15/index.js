b7f7080a9fff0a63685cd9f36fc8fe8791e35a8f
= <ismoilbek20050421@gmail.com>
2023-02-09 20:43:34 +0500

    Codewars to JavaScript || 8-kyu || Issues: N15

---
+++ 8-kyu/Issues-15/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// 8-kyu: Issues-15
+// #### Convert a string to an array
+
+const stringToArray = (array) => array.split(" ");
+stringToArray("Robbin Singh");
+
+// ### Write a function to split a string and convert it into an array of words.
+
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
