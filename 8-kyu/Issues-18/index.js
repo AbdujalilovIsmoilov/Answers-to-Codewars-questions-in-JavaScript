b4faf91e14a618c0609f86a98a505c33f20d9f97
= <ismoilbek20050421@gmail.com>
2023-02-12 13:37:28 +0500

    Codewars to JavaScript || 8-kyu || Issues: N18

---
+++ 8-kyu/Issues-18/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// 8-kyu: Issues-18
+// #### Reversed Words
+
+const reverseWords = (str) => str.split(" ").reverse().join(" ");
+reverseWords("The greatest victory is that which requires no battle");
+
+// ### Complete the solution so that it reverses all of the words within the string passed in.
+// ### Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
